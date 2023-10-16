e43ca4774ce59e5f92fa01ff4ddd0f079253289a
Zerkath <juha.airaksinen1@gmail.com>
2023-10-16 19:58:42 +0300

    feat: Reading pages from db

--- src/routes/Reports.svelte
+++ src/routes/Reports.svelte
@@ -1,1 +1,24 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+  import { onMount } from 'svelte';
+
+  let name: string[] = [];
+
+  onMount(() => {
+    invoke('query_tables').then((res) => {
+      name = res;
+    });
+  });
+
+	let tables: string[] = [];
+
+  console.log('Querying tables');
+
+</script>
+
 <p>Report View</p>
+<p>Hello {name}!</p>
+
+{#each tables as table}
+	<p>{table}</p>
+{/each}
